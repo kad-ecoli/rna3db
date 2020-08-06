bfa477c654dca647dc9808580715f2081eabec42
zcx <zcx@umich.edu>
2020-08-05 21:08:04 -0400

    seperate ideal DNA from ideal RNA

--- ligand-expo/createIdealDNA.py
+++ ligand-expo/createIdealDNA.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python
-''' create IdealRNA.hpp '''
+''' create IdealDNA.hpp '''
 from string import Template
 
 atom_template=Template('        ${RESN}["$ATOM"]=tmp; ${RESN}["$ATOM"][0]=$X; ${RESN}["$ATOM"][1]=$Y; ${RESN}["$ATOM"][2]=$Z;\n')
 atom2_template=Template('    ${RESN}["$ATOM"]=tmp; ${RESN}["$ATOM"][0]=$X; ${RESN}["$ATOM"][1]=$Y; ${RESN}["$ATOM"][2]=$Z;\n')
 
-hpp_txt='''/* IdealRNA.hpp - Idealized RNA nucleotide conformations */
+hpp_txt='''/* IdealDNA.hpp - Idealized DNA nucleotide conformations */
 #include <string>
 #include <map>
 #include <vector>
@@ -13,15 +13,15 @@ hpp_txt='''/* IdealRNA.hpp - Idealized RNA nucleotide conformations */
 using namespace std;
 
 /* option: 0 - ideal pdb; 1 - model pdb; 2 - 3dna fiber */
-map<string, map<string,vector<float> > >parse_ideal_rna(int option=2)
+map<string, map<string,vector<float> > >parse_ideal_dna(int option=2)
 {
     vector<float> tmp(3,0);
-    map<string, map<string,vector<float> > >ideal_rna;
+    map<string, map<string,vector<float> > >ideal_dna;
     if (option==0)
     {
 '''
 
-for resn in ['  A','  C','  G','  U']:
+for resn in [' DA',' DC',' DG',' DT']:
     fp=open("%s/%s_ideal.pdb"%(resn.strip(),resn.strip()),'r')
     lines=fp.read().splitlines()
     fp.close()
@@ -41,7 +41,7 @@ for resn in ['  A','  C','  G','  U']:
             Y=y,
             Z=z,
         )
-    hpp_txt+='''        ideal_rna["%s"]=%s;
+    hpp_txt+='''        ideal_dna["%s"]=%s;
         map<string,vector<float> >().swap(%s);
 
 '''%(resn,resn.strip(),resn.strip())
@@ -51,7 +51,7 @@ hpp_txt+='''    }
     {
 '''
 
-for resn in ['  A','  C','  G','  U']:
+for resn in [' DA',' DC',' DG',' DT']:
     fp=open("%s/%s_model.pdb"%(resn.strip(),resn.strip()),'r')
     lines=fp.read().splitlines()
     fp.close()
@@ -71,7 +71,7 @@ for resn in ['  A','  C','  G','  U']:
             Y=y,
             Z=z,
         )
-    hpp_txt+='''        ideal_rna["%s"]=%s;
+    hpp_txt+='''        ideal_dna["%s"]=%s;
         map<string,vector<float> >().swap(%s);
 
 '''%(resn,resn.strip(),resn.strip())
@@ -81,12 +81,12 @@ hpp_txt+='''    }
     {
 '''
 # generated using 
-# fiber -rna -seq=ACGU ACGU.rna.pdb
-fp=open("ACGU.rna.pdb",'r')
-lines=fp.read().splitlines()
+# fiber      -seq=ACGT ACGT.dna.pdb 
+fp=open("ACGT.dna.pdb",'r')
+lines+=fp.read().splitlines()
 fp.close()
 
-for resn in ['  A','  C','  G','  U']:
+for resn in [' DA',' DC',' DG',' DT']:
     hpp_txt+="        map<string,vector<float> >%s;\n"%(resn.strip())
     for line in lines:
         if not line.startswith("ATOM  ") or line[77]=='H' or \
@@ -103,7 +103,7 @@ for resn in ['  A','  C','  G','  U']:
             Y=y,
             Z=z,
         )
-    hpp_txt+='''        ideal_rna["%s"]=%s;
+    hpp_txt+='''        ideal_dna["%s"]=%s;
         map<string,vector<float> >().swap(%s);
 
 '''%(resn,resn.strip(),resn.strip())
@@ -112,14 +112,14 @@ hpp_txt+='''    }
 '''
 
 # generated using
-# fiber -rna -seq=AAACAGAUCACCCGCUGAGCGGGUUAUCUGUUAAAAACAAGAAUACAACCACGACUAGAAGCAGGAGUAUAAUCAUGAUUCAACACCAGCAUCCACCCCCGCCUCGACGCCGGCGUCUACUCCUGCUUGAAGACGAGGAUGCAGCCGCGGCUGGAGGCGGGGGUGUAGUCGUGGUUUAAUACUAGUAUUCAUCCUCGUCUUGAUGCUGGUGUUUAUUCUUGUUU 224.rna.pdb
+# fiber      -seq=AAACAGATCACCCGCTGAGCGGGTTATCTGTTAAAAACAAGAATACAACCACGACTAGAAGCAGGAGTATAATCATGATTCAACACCAGCATCCACCCCCGCCTCGACGCCGGCGTCTACTCCTGCTTGAAGACGAGGATGCAGCCGCGGCTGGAGGCGGGGGTGTAGTCGTGGTTTAATACTAGTATTCATCCTCGTCTTGATGCTGGTGTTTATTCTTGTTT 224.dna.pdb
 
-fp=open("224.rna.pdb",'r')
+fp=open("224.dna.pdb",'r')
 lines=fp.read().splitlines()
 fp.close()
 r=0
-for resn1 in ['  A','  C','  G','  U']:
-    for resn2 in ['  A','  C','  G','  U']:
+for resn1 in [' DA',' DC',' DG',' DT']:
+    for resn2 in [' DA',' DC',' DG',' DT']:
         key=resn1.strip()+resn2.strip()+'0'
         hpp_txt+="    map<string,vector<float> >%s;\n"%key
         r+=1
@@ -141,7 +141,7 @@ for resn1 in ['  A','  C','  G','  U']:
                 Y=y,
                 Z=z,
             )
-        hpp_txt+='''    ideal_rna["%s"]=%s;
+        hpp_txt+='''    ideal_dna["%s"]=%s;
     map<string,vector<float> >().swap(%s);
 
 '''%(resn1+resn2+'0',key,key)
@@ -167,14 +167,14 @@ for resn1 in ['  A','  C','  G','  U']:
                 Y=y,
                 Z=z,
             )
-        hpp_txt+='''    ideal_rna["%s"]=%s;
+        hpp_txt+='''    ideal_dna["%s"]=%s;
     map<string,vector<float> >().swap(%s);
 
 '''%(resn1+resn2+'1',key,key)
 
-for resn1 in ['  A','  C','  G','  U']:
-    for resn2 in ['  A','  C','  G','  U']:
-        for resn3 in ['  A','  C','  G','  U']:
+for resn1 in [' DA',' DC',' DG',' DT']:
+    for resn2 in [' DA',' DC',' DG',' DT']:
+        for resn3 in [' DA',' DC',' DG',' DT']:
             key=resn1.strip()+resn2.strip()+resn3.strip()+'0'
             hpp_txt+="    map<string,vector<float> >%s;\n"%key
             r+=1
@@ -196,7 +196,7 @@ for resn1 in ['  A','  C','  G','  U']:
                     Y=y,
                     Z=z,
                 )
-            hpp_txt+='''    ideal_rna["%s"]=%s;
+            hpp_txt+='''    ideal_dna["%s"]=%s;
     map<string,vector<float> >().swap(%s);
 
 '''%(resn1+resn2+resn3+'0',key,key)
@@ -222,7 +222,7 @@ for resn1 in ['  A','  C','  G','  U']:
                     Y=y,
                     Z=z,
                 )
-            hpp_txt+='''    ideal_rna["%s"]=%s;
+            hpp_txt+='''    ideal_dna["%s"]=%s;
     map<string,vector<float> >().swap(%s);
 
 '''%(resn1+resn2+resn3+'1',key,key)
@@ -248,16 +248,16 @@ for resn1 in ['  A','  C','  G','  U']:
                     Y=y,
                     Z=z,
                 )
-            hpp_txt+='''    ideal_rna["%s"]=%s;
+            hpp_txt+='''    ideal_dna["%s"]=%s;
     map<string,vector<float> >().swap(%s);
 
 '''%(resn1+resn2+resn3+'2',key,key)
 
 hpp_txt+='''    vector<float> ().swap(tmp);
-    return ideal_rna;
+    return ideal_dna;
 }
 '''
 
-fp=open("IdealRNA.hpp",'w')
+fp=open("IdealDNA.hpp",'w')
 fp.write(hpp_txt)
 fp.close()
