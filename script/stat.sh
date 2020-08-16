72ff712ae46d31fc6e60703fa980450c648855ff
zcx <zcx@umich.edu>
2020-08-16 08:27:47 -0400

    stat BP

--- script/stat.sh
+++ script/stat.sh
@@ -3,11 +3,16 @@ bindir=`dirname $(readlink -e $0)`
 rootdir=`dirname $bindir`
 culldir="$rootdir/cull"
 
-echo "L vs DSSR"
 mkdir -p $rootdir/stat
 cd $rootdir/stat
+
+echo "L vs DSSR"
 $bindir/statLvsDSSR.py $culldir/all_c1.0_s1.0/list.atomic $culldir/pdb_atom.sort.4.0_c0.8_s0.8 $culldir/all_c1.0_s1.0/DSSR/
 
 echo "NaTorsion"
 $bindir/batchNaTorsion.py $culldir/all_c1.0_s1.0/list.atomic $culldir/pdb_atom.sort.4.0_c0.8_s0.8 $culldir/all_c1.0_s1.0/
 $bindir/statNaTorsion.py NaTorsion.raw.gz
+
+echo "BPtorsion"
+$bindir/batchBPtorsion.py $culldir/all_c1.0_s1.0/list.atomic $culldir/pdb_atom.sort.4.0_c0.8_s0.8 $culldir/all_c1.0_s1.0/ $culldir/all_c1.0_s1.0/DSSR/
+$bindir/statBPtorsion.py BPtorsion.raw.gz
