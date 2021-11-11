ae56a9474339df53d5d9a8f7b000d21e4cc57ea5
Chengxin Zhang <cz378@farnam1>
2021-11-11 15:24:20 -0500

    dimer dbn

--- script/cull_dimer.sh
+++ script/cull_dimer.sh
@@ -24,17 +24,16 @@ all"
 
 cdhitest="$bindir/cd-hit-est -l 9 -r 0 -M 5000 -g 1"
 for resolu in $resolu_list;do
-    $cdhitest -i $dimerdir/pdb_atom.sort.${resolu}.fasta     -n 10 -o $dimerdir/pdb_atom.sort.${resolu}_c1.0_s1.0 -c 1.0 -s 1.0
-    $cdhitest -i $dimerdir/pdb_atom.sort.${resolu}_c1.0_s1.0 -n  9 -o $dimerdir/pdb_atom.sort.${resolu}_c0.9_s0.9 -c 0.9 -s 0.9
-    $cdhitest -i $dimerdir/pdb_atom.sort.${resolu}_c0.9_s0.9 -n  5 -o $dimerdir/pdb_atom.sort.${resolu}_c0.8_s0.8 -c 0.8 -s 0.8
+    $cdhitest -i $dimerdir/pdb_atom.sort.${resolu}.fasta     -n  8 -o $dimerdir/pdb_atom.sort.${resolu}_c1.0_s1.0 -c 1.0 -s 1.0
+    $cdhitest -i $dimerdir/pdb_atom.sort.${resolu}_c1.0_s1.0 -n  7 -o $dimerdir/pdb_atom.sort.${resolu}_c0.9_s0.9 -c 0.9 -s 0.9
+    $cdhitest -i $dimerdir/pdb_atom.sort.${resolu}_c0.9_s0.9 -n  4 -o $dimerdir/pdb_atom.sort.${resolu}_c0.8_s0.8 -c 0.8 -s 0.8
 
-    $cdhitest -i $dimerdir/pdb_atom.sort.${resolu}_c1.0_s1.0 -n 10 -o $dimerdir/pdb_atom.sort.${resolu}_c1.0_s0.0 -c 1.0
-    $cdhitest -i $dimerdir/pdb_atom.sort.${resolu}_c0.9_s0.9 -n  9 -o $dimerdir/pdb_atom.sort.${resolu}_c0.9_s0.0 -c 0.9
-    $cdhitest -i $dimerdir/pdb_atom.sort.${resolu}_c0.8_s0.8 -n  5 -o $dimerdir/pdb_atom.sort.${resolu}_c0.8_s0.0 -c 0.8
+    $cdhitest -i $dimerdir/pdb_atom.sort.${resolu}_c1.0_s1.0 -n  8 -o $dimerdir/pdb_atom.sort.${resolu}_c1.0_s0.0 -c 1.0
+    $cdhitest -i $dimerdir/pdb_atom.sort.${resolu}_c0.9_s0.9 -n  7 -o $dimerdir/pdb_atom.sort.${resolu}_c0.9_s0.0 -c 0.9
+    $cdhitest -i $dimerdir/pdb_atom.sort.${resolu}_c0.8_s0.8 -n  4 -o $dimerdir/pdb_atom.sort.${resolu}_c0.8_s0.0 -c 0.8
 done
 
 $bindir/makeCullTable.py $dimerdir
-
 
 echo "Clean PDB"
 resolu_cs_list="all_c1.0_s1.0"
@@ -48,23 +47,16 @@ for resolu_cs in $resolu_cs_list;do
     echo "remove files not listed by $PWD/list"
     for target in `ls *.pdb|cut -f1 -d.`;do
         GREP=`grep $target list`
-	if [ -z "$GREP" ];then
-	    echo rm $target.pdb
-	    rm $target.pdb
-	    rm DSSR/$target.dssr
-	fi
+        if [ -z "$GREP" ];then
+            echo rm $target.pdb
+            rm $target.pdb
+            rm DSSR/$target.dssr
+            rm DSSR/$target.dbn
+        fi
     done
     
-    echo "update $PWD/$list.new"
-    for target in `cat list`;do
-        if [ ! -s "${target}.pdb" ];then
-	    echo $target
-	fi
-    done > list.new
-    
     echo "generate $PWD/*.pdb"
-    $bindir/cleanDimerPdb.py -dir=$rootdir/pdb/data/structures/all/pdb/ list.new -suffix=.pdb.gz .pdb
-    rm list.new
+    $bindir/cleanDimerPdb.py -dir=$rootdir/pdb/data/structures/all/pdb/ list -suffix=.pdb.gz .pdb
 
     echo "generate $PWD/list.atomic"
     $bindir/AtomsPerResidue.py -dir=./ list -suffix=.pdb|grep -P "^[:\w]+\s[2]\d\.\d{2}$"|cut -f1 > list.atomic
@@ -76,11 +68,11 @@ for resolu_cs in $resolu_cs_list;do
 	    $bindir/x3dna-dssr -i=$target.pdb --pair-only -o=DSSR/$target.dssr
 	fi
     done
-    
+
     echo "fill missing atoms"
     cd $rootdir/dimer/$resolu_cs
     for target in `cat list`;do
-	$bindir/MissingRNAatom ${target}.pdb ${target}.pdb 5
+        $bindir/MissingRNAatom ${target}.pdb ${target}.pdb 5
     done
     $bindir/clstr2txt.py $rootdir/dimer/pdb_atom.sort.${resolu_cs}.clstr $rootdir/dimer/pdb_atom.sort.${resolu_cs}.txt
     $bindir/pdb2xyz -dir ./ list -suffix .pdb -atom " C3'" > C3.xyz
@@ -90,5 +82,43 @@ for resolu_cs in $resolu_cs_list;do
     $bindir/pdb2xyz -dir ./ list -suffix .pdb -atom " O3'" > O3.xyz
     $bindir/pdb2xyz -dir ./ list -suffix .pdb -atom " P  " >  P.xyz
     cd $rootdir/dimer
-    tar -cjvf ${resolu_cs}.tar.bz2 ${resolu_cs}/list* ${resolu_cs}/*.pdb ${resolu_cs}/*.xyz pdb_atom.sort.${resolu_cs}.txt
+    cut -f1 $rootdir/dimer/pdb_atom.sort.${resolu_cs} | $bindir/fastaOneLine - $rootdir/dimer/$resolu_cs/list.fasta
+    tar -cjvf ${resolu_cs}.tar.bz2 ${resolu_cs}/list* ${resolu_cs}/*.pdb ${resolu_cs}/*.xyz pdb_atom.sort.${resolu_cs}.txt ${resolu_cs}/list.fasta
+    
+    echo "generate dot bracket"
+    cd $rootdir/dimer/$resolu_cs/DSSR
+    for target in `cat $rootdir/dimer/$resolu_cs/list.atomic`;do
+        if [ -s "$target.dbn" ];then
+            continue
+        fi
+        $bindir/x3dna-dssr -i=$rootdir/dimer/$resolu_cs/$target.pdb
+        Ldbn=`tail -1 dssr-2ndstrs.dbn|sed 's/&//g'|wc -c`
+        Lseq=`grep -A1 ">$target$" $rootdir/dimer/$resolu_cs/list.fasta|tail -1 |wc -c`
+        if [ $Lseq -eq $Ldbn ];then
+            tail -1 dssr-2ndstrs.dbn | sed 's/&//g' | sed 's/[A-Za-z{}\[<>]/./g' | sed 's/]/./g' > $target.dbn
+        fi
+        rm dssr-*
+    done
+    
+    echo "re-copy $PWD/*.pdb"
+    cd $rootdir/dimer/$resolu_cs
+    $bindir/cleanDimerPdb.py -dir=$rootdir/pdb/data/structures/all/pdb/ list -suffix=.pdb.gz .pdb
+    cd $rootdir/dimer/$resolu_cs/DSSR
+    for target in `cat $rootdir/dimer/$resolu_cs/list`;do
+        if [ -s "$target.dbn" ];then
+            continue
+        fi
+        $bindir/cssr $rootdir/dimer/$resolu_cs/$target.pdb 1 | sed 's/[\[{}()]/./g'|sed 's/]/./g'  > $target.dbn
+    done
+done
+
+echo "make smaller subsets"
+resolu_cs_list="all_c1.0_s1.0 all_c0.9_s0.9 all_c0.8_s0.8 all_c0.8_s0.0"
+
+for resolu_cs in $resolu_cs_list;do
+    echo $resolu_cs
+    mkdir -p $rootdir/dimer/$resolu_cs
+    grep '>' $rootdir/dimer/pdb_atom.sort.$resolu_cs|cut -f1|sed 's/>//g' > $rootdir/dimer/$resolu_cs/list
+    $bindir/clstr2txt.py $rootdir/dimer/pdb_atom.sort.${resolu_cs}.clstr $rootdir/dimer/pdb_atom.sort.${resolu_cs}.txt
+    cut -f1 $rootdir/dimer/pdb_atom.sort.${resolu_cs} > $rootdir/dimer/$resolu_cs/list.fasta
 done
